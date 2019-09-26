516e1a1cd9dc1f00a6de8a66fc493b27ac5cf9b7
Rother <Lorena.Rother@Mattel.com>
2019-09-26 14:18:41 -0700

    Put code inside function. Warn one other plane is needed if no axes are aligned with world axes

--- scripts/lr_ctrlOrientTool.py
+++ scripts/lr_ctrlOrientTool.py
@@ -1,3 +1,10 @@
+####################################
+##  CTRL ORIENT TOOL              ##
+##  lr_ctrlOrientTool.py          ##
+##  Created by Lorena Rother      ##
+##  Updated: 24 Sep 2019          ##
+####################################
+
 import maya.cmds as mc
 import math 
 import maya.OpenMaya as om
@@ -5,52 +12,72 @@ import maya.OpenMaya as om
 def warning_msg(msg, arg=None):
     mc.confirmDialog(title='Warning', message=msg, button='ok')
 
-sel =  mc.ls(sl=True, fl=1)
-if(sel):
-    ctrl = sel.pop()
-    if(len(verts)==3 or len(verts)==6):
+def create_orient_group():
+    #turn on track selection order
+    if ( not mc.selectPref(trackSelectionOrder=1, q=1)):
+        mc.selectPref(trackSelectionOrder=True)
+    sel =  mc.ls(orderedSelection=True)
+
+    if(sel):
+        ctrl = sel.pop()
         verts = sel
-        #create orient grp with same pivot as ctrl then parent ctrl
-        orientGrp = mc.group(n=str(ctrl).replace('_Ctrl', '_')+'orientGrp', em=1)
-        constraint = mc.parentConstraint( ctrl, orientGrp)
-        mc.delete(constraint)
-        mc.parent(ctrl, orientGrp)
+        print("verts: ", verts)
+        if(len(verts)==3 or len(verts)==6):
+            #create helper plane/s. Find normals 
+            helper_plane_01 = mc.polyCreateFacet( p=[mc.pointPosition(verts[0]), mc.pointPosition(verts[1]), mc.pointPosition(verts[2])] )
+            plane01_str_normal = mc.polyInfo(fn=1)[0].split()
+            mc.delete(helper_plane_01)
+            plane01_normal = (float(plane01_str_normal[2]), float(plane01_str_normal[3]), float(plane01_str_normal[4]))
+            vectorN = om.MVector(plane01_normal[0], plane01_normal[1], plane01_normal[2])
 
-        #create helper plane/s. Find normals 
-        helper_plane_01 = mc.polyCreateFacet( p=[mc.pointPosition(verts[0]), mc.pointPosition(verts[1]), mc.pointPosition(verts[2])] )
-        plane01_str_normal = mc.polyInfo(fn=1)[0].split()
-        mc.delete(helper_plane_01)
-        plane01_normal = (float(plane01_str_normal[2]), float(plane01_str_normal[3]), float(plane01_str_normal[4]))
-        vectorN = om.MVector(plane01_normal[0], plane01_normal[1], plane01_normal[2])
+            if (len(verts)==6):
+                helper_plane_02 = mc.polyCreateFacet( p=[mc.pointPosition(verts[3]), mc.pointPosition(verts[4]), mc.pointPosition(verts[5])] )
+                plane02_str_normal = mc.polyInfo(fn=1)[0].split()
+                mc.delete(helper_plane_02)
+                plane02_normal = (float(plane02_str_normal[2]), float(plane02_str_normal[3]), float(plane02_str_normal[4]))
+                vectorT = om.MVector(plane02_normal[0], plane02_normal[1], plane02_normal[2])
+            else:
+                # if only one plane is provided, find which one of the world axes is orthogonal to the normal
+                if (vectorN*om.MVector(1,0,0) == 0):
+                    vectorT = om.MVector(1,0,0)
+                elif (vectorN*om.MVector(0,1,0) == 0):
+                    vectorT = om.MVector(0,1,0)
+                elif (vectorN*om.MVector(0,0,1) == 0):
+                    vectorT = om.MVector(0,0,1)
+                else:
+                    #if none of the axes are aligned with a world axis, ask for more verts to define a 2nd plane
+                    warning_msg("No axes are aligned with a world axis. You'll need to select 6 vertices and then ctrl.")
+                    return
 
-        if (len(verts)==6):
-            helper_plane_02 = mc.polyCreateFacet( p=[mc.pointPosition(verts[3]), mc.pointPosition(verts[4]), mc.pointPosition(verts[5])] )
-            plane02_str_normal = mc.polyInfo(fn=1)[0].split()
-            mc.delete(helper_plane_02)
-            plane02_normal = (float(plane02_str_normal[2]), float(plane02_str_normal[3]), float(plane02_str_normal[4]))
-            vectorT = om.MVector(plane02_normal[0], plane02_normal[1], plane02_normal[2])
+            #find 3rd vector
+            vectorX = vectorN^vectorT
+
+            #create orient grp with same pivot as ctrl then parent ctrl
+            orientGrp = mc.group(n=str(ctrl).replace('_Ctrl', '_')+'orientGrp', em=1)
+            constraint = mc.parentConstraint( ctrl, orientGrp)
+            mc.delete(constraint)
+            mc.parent(ctrl, orientGrp)
+
+            #make rotation matrix from vectors
+            #keep position and assign rotation matrix to orientGrp 
+            orientGrp_matrix = mc.xform(orientGrp,q=1, m=1)
+            mc.xform(orientGrp, m=(vectorX.x, vectorX.y, vectorX.z, 0, vectorN.x, vectorN.y, vectorN.z, 0, vectorT.x, vectorT.y, vectorT.z, 0,orientGrp_matrix[12],orientGrp_matrix[13],orientGrp_matrix[14],orientGrp_matrix[15] ))
+            #freeze transforms
+            mc.makeIdentity(orientGrp, apply=1, t=1, n=0)
         else:
-            # if only one plane is provided, find which one of the world axes is orthogonal to the normal
-            if (vectorN*om.MVector(1,0,0) == 0):
-                vectorT = om.MVector(1,0,0)
-            if (vectorN*om.MVector(0,1,0) == 0):
-                vectorT = om.MVector(0,1,0)
-            if (vectorN*om.MVector(0,0,1) == 0):
-                vectorT = om.MVector(0,0,1)
-            print(vectorT.x, vectorT.y, vectorT.z)
-
-        vectorX = vectorN^vectorT
-
-        #make rotation matrix from vectors
-        #keep position and assign rotation matrix to orientGrp 
-        orientGrp_matrix = mc.xform(orientGrp,q=1, m=1)
-        mc.xform(orientGrp, m=(vectorX.x, vectorX.y, vectorX.z, 0, vectorN.x, vectorN.y, vectorN.z, 0, vectorT.x, vectorT.y, vectorT.z, 0,orientGrp_matrix[12],orientGrp_matrix[13],orientGrp_matrix[14],orientGrp_matrix[15] ))
-        #freeze transforms
-        mc.makeIdentity(orientGrp, apply=1, t=1, n=0)
+            warning_msg("Please select 3 or 6 vertices and then ctrl")
+            return
     else:
         warning_msg("Please select 3 or 6 vertices and then ctrl")
-else:
-    warning_msg("Please select 3 or 6 vertices and then ctrl")
+        return
+
+####################################
+##  CTRL ORIENT TOOL              ##
+##  lr_ctrlOrientTool.py          ##
+##  Created by Lorena Rother      ##
+##  Updated: 24 Sep 2019          ##
+####################################
+
 
 
 
